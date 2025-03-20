5b245e9d8aef44e228f6286eb880418f8991ff97
saramohsen18165 <mohsensarah790@gmail.com>
2025-03-20 20:11:47 +0000

    sec commit

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,6 +1,20 @@
 import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
+
+
+
+
+  {
+    path:"check-out/:cartId",
+    renderMode:RenderMode.Server
+  },
+
+  {
+    path:"product-details/:id",
+    renderMode:RenderMode.Server
+  },
+
   {
     path: '**',
     renderMode: RenderMode.Prerender
